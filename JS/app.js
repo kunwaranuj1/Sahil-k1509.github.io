919c77201828876b53c2c9c67fd7902e1ba1b1d5
Sahil Bairagi <sahilbairagi319@gmail.com>
2021-12-22 19:41:02 +0530

    added chess section

--- JS/app.js
+++ JS/app.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
     var entriesL = [  {target: '_top',url: '#about', label: 'Terminal'}, {target: '_top',url: '#about', label: 'Pycharm'},  {target: '_top',url: '#about', label: 'VS Code'},  {target: '_top',url: '#about', label: 'Jupyter Notebook'}, {target: '_top',url: '#about', label: 'Vim'}, {target: '_top',url: '#about', label: 'Git'},  {target: '_top',url: '#about', label: 'Github'}, {target: '_top',url: '#about', label: 'Javascript'},  {target: '_top',url: '#about', label: 'Python'},  {target: '_top',url: '#about', label: 'PHP'},  {target: '_top',url: '#about', label: 'Go'},  {target: '_top',url: '#about', label: 'SQL'},  {target: '_top',url: '#about', label: 'API'}, {target: '_top',url: '#about', label: 'Object Oriented Programming'},   {target: '_top',url: '#about', label: 'C++'},  {target: '_top',url: '#about', label: 'C'},  {target: '_top',url: '#about', label: 'Data Structures'},  {target: '_top',url: '#about', label: 'Algorithms'},  {target: '_top',url: '#about', label: 'Batch Scripting'},  {target: '_top',url: '#about', label: 'JSON'}, ];
     var entriesW = [  {target: '_top',url: '#about', label: 'HTML'},  {target: '_top',url: '#about', label: 'CSS'},  {target: '_top',url: '#about', label: 'SASS'},  {target: '_top',url: '#about', label: 'JQuery'},  {target: '_top',url: '#about', label: 'PHP'},  {target: '_top',url: '#about', label: 'Ajax'},  {target: '_top',url: '#about', label: 'ReactJS'},  {target: '_top',url: '#about', label: 'Bootstrap'}, {target: '_top',url: '#about', label: 'Django'},  {target: '_top',url: '#about', label: 'MySql'},  {target: '_top',url: '#about', label: 'MongoDB'},  {target: '_top',url: '#about', label: 'Flask'},  {target: '_top',url: '#about', label: 'Responsive Design'},  {target: '_top',url: '#about', label: 'MVC model'},];
     var entriesM = [  {target: '_top',url: '#about', label: 'Regression'},  {target: '_top',url: '#about', label: 'Classification'},  {target: '_top',url: '#about', label: 'Tensorflow'},  {target: '_top',url: '#about', label: 'Scikit Learn'},  {target: '_top',url: '#about', label: 'Numpy'},  {target: '_top',url: '#about', label: 'Pandas'},  {target: '_top',url: '#about', label: 'Matplotlib'},  {target: '_top',url: '#about', label: 'Seaborn'},  {target: '_top',url: '#about', label: 'Convulational NN'},];
-    var entriesT = [  {target: '_top',url: '#about', label: 'PyGame'}, {target: '_top',url: '#about', label: 'Tkinter'},  {target: '_top',url: '#about', label: 'Turtle Graphics'},  {target: '_top',url: '#about', label: 'Scrapy'},  {target: '_top',url: '#about', label: 'Exception Handling'},  {target: '_top',url: '#about', label: 'Multithreading'},  {target: '_top',url: '#about', label: 'OS, Sys, Subprocess'},  {target: '_top',url: '#about', label: 'Beautiful Soup'},  {target: '_top',url: '#about', label: 'OpenCV'},  {target: '_top',url: '#about', label: 'Sockets'}, {target: '_top',url: '#about', label:'Translate'}, {target: '_top',url: '#about', label:'Selenium'}, {target: '_top',url: '#about', label:'SymPy'},  {target: '_top',url: '#about', label:'SMTP'}, {target: '_top',url: '#about', label:'Regular Expressions'}, {target: '_top',url: '#about', label:'Logging'},  {target: '_top',url: '#about', label:'Generators'}, {target: '_top',url: '#about', label:'Context Managers'},  {target: '_top',url: '#about', label:'Decorators'},  {target: '_top',url: '#about', label:'Requests'}];
+    var entriesT = [  {target: '_top',url: '#about', label: 'PyGame'}, {target: '_top',url: '#about', label: 'Tkinter'},  {target: '_top',url: '#about', label: 'Turtle Graphics'},  {target: '_top',url: '#about', label: 'Scrapy'},  {target: '_top',url: '#about', label: 'Exception Handling'},  {target: '_top',url: '#about', label: 'Beautiful Soup'},  {target: '_top',url: '#about', label: 'OpenCV'},  {target: '_top',url: '#about', label:'Selenium'}, {target: '_top',url: '#about', label:'Requests'}];
 
     
     rval = document.getElementById('r-set');
@@ -448,5 +448,14 @@ $(document).ready(function(){
 
     init();
     animate();
+
+
+    $('input[type=radio][name=gameno]').change(function(){
+        let value = this.value;
+        let string = `./Media/game${value}.gif`;
+        let element = document.getElementById('game-box');
+        element.removeChild(element.firstChild);
+        element.innerHTML = `<img src='${string}' class='game-window' />`
+    })
 
 });
